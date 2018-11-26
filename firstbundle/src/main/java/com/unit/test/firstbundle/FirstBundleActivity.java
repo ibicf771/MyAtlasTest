3abdd869530eac6857b930ee763c448fa649557f
yangsimin <181326622@qq.com>
2018-11-26 16:02:39 +0800

    myAtlas 增加module

--- firstbundle/src/main/java/com/unit/test/firstbundle/FirstBundleActivity.java
+++ firstbundle/src/main/java/com/unit/test/firstbundle/FirstBundleActivity.java
@@ -1,8 +1,12 @@
 package com.unit.test.firstbundle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.taobao.secondbundlelibrary.SecondbundleShareActivity;
 
 /**
  * Created by yangsimin on 2018/11/26.
@@ -13,5 +17,18 @@ public class FirstBundleActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_bundle_acticity);
+        findViewById(R.id.second_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                startActivity(new Intent(FirstBundleActivity.this, SecondActivity.class));
+//                Intent intent = new Intent();
+//                intent.setClass(FirstBundleActivity.this, SecondbundleShareActivity.class);
+//                startActivity(intent);
+
+                Intent intent = new Intent();
+                intent.setClass(FirstBundleActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
