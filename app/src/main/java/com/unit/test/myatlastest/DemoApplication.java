6e778a76c22afe461267f6bdc3a64471b8a949ea
yangsimin <181326622@qq.com>
2018-11-27 10:25:32 +0800

    gradle4.1的版本

--- app/src/main/java/com/unit/test/myatlastest/DemoApplication.java
+++ app/src/main/java/com/unit/test/myatlastest/DemoApplication.java
@@ -20,48 +20,90 @@ import java.io.File;
  */
 
 public class DemoApplication extends Application {
+//
+//    @Override
+//    public void onCreate() {
+//
+//        super.onCreate();
+//
+//        Atlas.getInstance().setClassNotFoundInterceptorCallback(new ClassNotFoundInterceptorCallback() {
+//            @Override
+//            public Intent returnIntent(Intent intent) {
+//                final String className = intent.getComponent().getClassName();
+//                final String bundleName = AtlasBundleInfoManager.instance().getBundleForComponet(className);
+//
+//                if (!TextUtils.isEmpty(bundleName) && !AtlasBundleInfoManager.instance().isInternalBundle(bundleName)) {
+//
+//                    //远程bundle
+//                    Activity activity = ActivityTaskMgr.getInstance().peekTopActivity();
+//                    File remoteBundleFile = new File(activity.getExternalCacheDir(),"lib" + bundleName.replace(".","_") + ".so");
+//
+//                    String path = "";
+//                    if (remoteBundleFile.exists()){
+//                        path = remoteBundleFile.getAbsolutePath();
+//                    }else {
+//                        Toast.makeText(activity, " 远程bundle不存在，请确定 : " + remoteBundleFile.getAbsolutePath() , Toast.LENGTH_LONG).show();
+//                        return intent;
+//                    }
+//
+//
+//                    PackageInfo info = activity.getPackageManager().getPackageArchiveInfo(path, 0);
+//                    try {
+//                        Atlas.getInstance().installBundle(info.packageName, new File(path));
+//                    } catch (BundleException e) {
+//                        Toast.makeText(activity, " 远程bundle 安装失败，" + e.getMessage() , Toast.LENGTH_LONG).show();
+//
+//                        e.printStackTrace();
+//                    }
+//
+//                    activity.startActivities(new Intent[]{intent});
+//
+//                }
+//
+//                return intent;
+//            }
+//        });
+//
+//    }
+
 
     @Override
     public void onCreate() {
 
         super.onCreate();
+        Atlas.getInstance().setClassNotFoundInterceptorCallback(intent -> {
+            final String className = intent.getComponent().getClassName();
+            final String bundleName = AtlasBundleInfoManager.instance().getBundleForComponet(className);
 
-        Atlas.getInstance().setClassNotFoundInterceptorCallback(new ClassNotFoundInterceptorCallback() {
-            @Override
-            public Intent returnIntent(Intent intent) {
-                final String className = intent.getComponent().getClassName();
-                final String bundleName = AtlasBundleInfoManager.instance().getBundleForComponet(className);
+            if (!TextUtils.isEmpty(bundleName) && !AtlasBundleInfoManager.instance().isInternalBundle(bundleName)) {
 
-                if (!TextUtils.isEmpty(bundleName) && !AtlasBundleInfoManager.instance().isInternalBundle(bundleName)) {
+                //远程bundle
+                Activity activity = ActivityTaskMgr.getInstance().peekTopActivity();
+                File remoteBundleFile = new File(activity.getExternalCacheDir(),"lib" + bundleName.replace(".","_") + ".so");
 
-                    //远程bundle
-                    Activity activity = ActivityTaskMgr.getInstance().peekTopActivity();
-                    File remoteBundleFile = new File(activity.getExternalCacheDir(),"lib" + bundleName.replace(".","_") + ".so");
-
-                    String path = "";
-                    if (remoteBundleFile.exists()){
-                        path = remoteBundleFile.getAbsolutePath();
-                    }else {
-                        Toast.makeText(activity, " 远程bundle不存在，请确定 : " + remoteBundleFile.getAbsolutePath() , Toast.LENGTH_LONG).show();
-                        return intent;
-                    }
-
-
-                    PackageInfo info = activity.getPackageManager().getPackageArchiveInfo(path, 0);
-                    try {
-                        Atlas.getInstance().installBundle(info.packageName, new File(path));
-                    } catch (BundleException e) {
-                        Toast.makeText(activity, " 远程bundle 安装失败，" + e.getMessage() , Toast.LENGTH_LONG).show();
-
-                        e.printStackTrace();
-                    }
-
-                    activity.startActivities(new Intent[]{intent});
-
+                String path = "";
+                if (remoteBundleFile.exists()){
+                    path = remoteBundleFile.getAbsolutePath();
+                }else {
+                    Toast.makeText(activity, " 远程bundle不存在，请确定 : " + remoteBundleFile.getAbsolutePath() , Toast.LENGTH_LONG).show();
+                    return intent;
                 }
 
-                return intent;
+
+                PackageInfo info = activity.getPackageManager().getPackageArchiveInfo(path, 0);
+                try {
+                    Atlas.getInstance().installBundle(info.packageName, new File(path));
+                } catch (BundleException e) {
+                    Toast.makeText(activity, " 远程bundle 安装失败，" + e.getMessage() , Toast.LENGTH_LONG).show();
+
+                    e.printStackTrace();
+                }
+
+                activity.startActivities(new Intent[]{intent});
+
             }
+
+            return intent;
         });
 
     }
