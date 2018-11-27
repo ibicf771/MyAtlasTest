3389f9fb911996c665f13203362c9b1020eed5df
yangsimin <181326622@qq.com>
2018-11-27 15:06:51 +0800

    统一26 去掉secondbundlelibrary

--- firstbundle/src/main/java/com/unit/test/firstbundle/FirstBundleActivity.java
+++ firstbundle/src/main/java/com/unit/test/firstbundle/FirstBundleActivity.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.taobao.secondbundlelibrary.SecondbundleShareActivity;
 
 /**
  * Created by yangsimin on 2018/11/26.
