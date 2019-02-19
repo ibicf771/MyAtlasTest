86fd49bf7431cbf8a7e9dc1574f9845d158dc81d
yangsimin <181326622@qq.com>
2019-02-19 14:17:42 +0800

    gradel4.1编译暂时通过

--- firstbundle/src/main/java/com/unit/test/firstbundle/FirstBundleActivity.java
+++ firstbundle/src/main/java/com/unit/test/firstbundle/FirstBundleActivity.java
@@ -5,8 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
-import com.taobao.secondbundlelibrary.SecondbundleShareActivity;
+//
+//import com.taobao.secondbundlelibrary.SecondbundleShareActivity;
 
 /**
  * Created by yangsimin on 2018/11/26.
